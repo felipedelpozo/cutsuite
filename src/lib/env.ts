a7053c45bca2b650c005a24b4d11246384172db6
Felipe del Pozo <felipedelpozo@dtoo.es>
2025-04-05 22:02:31 +0200

    auth dashboard

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,6 +8,8 @@ const env = createEnv({
     DATABASE_URL: z.string().url(),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
+    EVOLUTION_API_URL: z.string().url(),
+    EVOLUTION_API_KEY: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
@@ -16,6 +18,8 @@ const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    EVOLUTION_API_URL: process.env.EVOLUTION_API_URL,
+    EVOLUTION_API_KEY: process.env.EVOLUTION_API_KEY,
   },
 });
 
