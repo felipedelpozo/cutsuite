25008b9e96ad31ac6476ca51f79a1e5aa7834940
Felipe del Pozo <felipedelpozo@dtoo.es>
2025-04-05 20:33:03 +0200

    auth and enviroment sample

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,14 +3,16 @@ import { z } from 'zod';
 
 const env = createEnv({
   server: {
-    AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().url(),
     DATABASE_URL: z.string().url(),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
