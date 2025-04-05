fc2e2d8c8edb2fb8533bec6639bdfc841986f20b
Felipe del Pozo <felipedelpozo@dtoo.es>
2025-04-05 15:03:17 +0200

    auth and middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
 import { getTranslations } from 'next-intl/server';
+
+import { Button } from '@/components/ui/button';
 
 export default async function Home() {
   const t = await getTranslations('HomePage');
 
-  return <h1>{t('title')}</h1>;
+  return (
+    <div className="h-svh">
+      <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-[7rem] leading-tight font-bold">{t('title')}</h1>
+        <div className="mt-6 flex gap-4">
+          <Button asChild>
+            <Link href="/dashboard">{t('goDashboard')}</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
